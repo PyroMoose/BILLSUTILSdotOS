9f231ac4018ec1efb9abeba73a584a8d3f8a9f27
opensesame11 <ceostrikeco@gmail.com>
2015-03-15 18:27:03 -0300

    just saving my prograss :P

--- bootloader.asm
+++ bootloader.asm
@@ -1,10 +1,19 @@
+[BITS 16]
+
 jmp bootloader
 
-;bootsector info goes here
+;Insert bootsector info here
 
 bootloader:
 
-jmp bootloader
+mov ax, 0x07c0
+mov ds, ax
+
+hang:
+jmp hang
+
+
+
 
 times 510-($-$$) db 0
 dw 0xAA55
