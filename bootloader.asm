98c718b00344920a117d4f703ddbc87f3f2e03b3
opensesame11 <ceostrikeco@gmail.com>
2015-03-15 20:53:28 -0300

    Added bootsector info

--- bootloader.asm
+++ bootloader.asm
@@ -2,18 +2,32 @@
 
 jmp bootloader
 
-;Insert bootsector info here
+OEMLabel			db "        "
+BytesPerSector		dw 512
+SectorsPerCluster	db 1
+ReservedForBoot		dw 1
+NumberOfFats		db 2
+RootDirEntries		dw 224
+LogicalSectors		dw 2880
+MediaDesctiptor		db 0F0h
+SectorsPerFat		dw 9
+SectorsPerTrack		dw 18
+Sides				dw 2
+HiddenSectors		dd 0
+LargeSectors		dd 0
+DriveNo				dw 0
+Signature			db 41
+VolumeID			dd 00000000h
+VolumeLabel			db "BILLSUTILS"
+FileSystem			db "FAT12   "
 
 bootloader:
 
 mov ax, 0x07c0
 mov ds, ax
-
+mov ax, 0
 hang:
 jmp hang
-
-
-
 
 times 510-($-$$) db 0
 dw 0xAA55
