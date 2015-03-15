fa8a5e079903bc5c1d1f3721d7042aa6a9163129
opensesame11 <ceostrikeco@gmail.com>
2015-03-15 18:36:41 -0300

    fixed some weird issues with script
    It should be fine now. Some spelling and syntax errors. Compiles fine
    now.

--- build.bat
+++ build.bat
@@ -9,16 +9,14 @@ echo Assembling kernel...
 nasm -O0 -f bin -o kernel.bin kernel.asm
 
 echo Assembling programs...
-cd ..\programs
- for %%i in (*.asm) do nasm -O0 -fbin %%i
- for %%i in (*.bin) do del %%i
- for %%i in (*.) do ren %%i %%i.bin
+cd programs
+for %%i in (*.asm) do nasm -O0 -fbin %%i
+for %%i in (*.bin) do del %%i
+for %%i in (*.) do ren %%i %%i.bin
 cd ..
 
 echo Adding bootsector to disk image...
-cd disk_images
-msdos partcopy ..\bootloader.bin 0 200 BILLSUTILSdotOS.flp 0
-cd ..
+msdos partcopy bootloader.bin 0 200 .\disk_images\BILLSUTILSdotOS.flp 0
 
 echo Mounting disk image...
 imdisk -a -f .\disk_images\BILLSUTILSdotOS.flp -s 1440K -m B:
